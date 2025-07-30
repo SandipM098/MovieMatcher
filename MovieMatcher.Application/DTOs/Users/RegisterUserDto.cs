80e8834f173be45732ba8b28c01457df1ed77384
Sandip Magar <reone054@gmail.com>
2025-07-30 17:22:14 +0545

    data annotation for register

--- MovieMatcher.Application/DTOs/Users/RegisterUserDto.cs
+++ MovieMatcher.Application/DTOs/Users/RegisterUserDto.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieMatcher.Application.DTOs.Users
 {
     public class RegisterUserDto
     {
+        [MaxLength(20, ErrorMessage = "User name must be less than 20 characters")]
+        [Required(ErrorMessage = "User name is required")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
+        [MaxLength(100, ErrorMessage = "Password must be less than 100 characters")]
+        [RegularExpression(
+            @"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=]).{6,100}$",
+            ErrorMessage = "Password must contain at least one uppercase letter, one number, and one special character.")]
         public string PasswordHash { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password")]
+        [Compare("PasswordHash", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
     }
 }
--- MovieMatcher.Shared/obj/Debug/net8.0/MovieMatcher.Shared.AssemblyInfo.cs
+++ MovieMatcher.Shared/obj/Debug/net8.0/MovieMatcher.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieMatcher.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15f6f195c4036e09120c2bef5c088180f77bc345")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5978f416a07450071e689b3e33072f7e6d2872f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieMatcher.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieMatcher.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
