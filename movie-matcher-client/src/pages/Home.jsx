ea20dd17f03b04bfda73647354225d0bb41e6a0a
Sandip Magar <reone054@gmail.com>
2025-08-08 16:48:32 +0545

    movie card with grid

--- movie-matcher-client/src/pages/Home.jsx
+++ movie-matcher-client/src/pages/Home.jsx
@@ -6,16 +6,22 @@ import { MovieCard } from "./movies/MovieCard";
 const Home = () => {
   return (
     <>
-    <div className="hidden sm:block">
-      <Header />
-      <div className="p-10">
-        <MovieCard/>
+      <div className="hidden sm:block">
+        <Header />
+        <div className="p-10 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
+          {Array.from({ length: 10 }).map((_, i) => (
+            <MovieCard key={i} />
+          ))}
+        </div>
       </div>
-    </div>
       <div className="block sm:hidden">
-       <Search/>
-        Home
-        <Footer/>
+        <Search />
+        <div className="p-10 grid grid-cols-2 gap-6">
+          {Array.from({ length: 10 }).map((_, i) => (
+            <MovieCard key={i} />
+          ))}
+        </div>
+        <Footer />
       </div>
     </>
   );
--- movie-matcher-client/src/pages/movies/MovieCard.jsx
+++ movie-matcher-client/src/pages/movies/MovieCard.jsx
@@ -2,14 +2,13 @@ import React from "react";
 
 export const MovieCard = () => {
   return (
-    <div className="relative border border-black h-60 w-60 rounded overflow-hidden">
+    <div className="border border-black h-56 w-56 rounded overflow-hidden relative group">
       <img
         alt="Movie Poster"
-        className="w-full h-full object-cover"
+        className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-300"
         src="/images/default-image.webp"
       />
-
-      <div className="absolute bottom-2 right-2 z-[900] bg-white text-black px-3 py-1 text-sm rounded shadow-lg hover:bg-gray-800 cursor-pointer transition">
+      <div className="absolute bottom-2 left-2 right-2 z-[900] bg-white text-black px-3 py-1 text-sm rounded shadow-lg hover:bg-gray-800 hover:text-white cursor-pointer transition-all duration-300">
         Download
       </div>
     </div>
